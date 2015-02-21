2253ebbabc19c7c67e627463a78c17ff7baffa6f
Bhaskar <Bhaskarnotbhaskar@gmail.com>
2015-02-22 03:02:40 +0530

    info and comments added

--- machine_learning/regression.py
+++ machine_learning/regression.py
@@ -9,49 +9,68 @@ class GenerateData:
 		self.data=[]
 	def initialize(self,n):
 		for x in range(n):
-			self.data.append((float(random.randint(1,1000)),float(random.randint(1,20000))))
+			self.data.append((float(random.randint(1,10)),float(random.randint(1,10))))
 	
 #a=GenerateData()
 #a.initialize()
 #print a.data
 
 def cost(lis,theta0,theta1):
+	'''fn to find the cost'''
 	cost=0.0
-	print theta0,theta1
+	print "theta0",theta0
+	print "theta1",theta1
 	for x in range(len(lis)):
-		cost=cost+((theta0+theta1*lis[x][0]-lis[x][1])*(theta0+theta1*lis[x][0]-lis[x][1]))/2*len(lis)
+		cost=cost+(((theta0+theta1*lis[x][0]-lis[x][1])*(theta0+theta1*lis[x][0]-lis[x][1]))/2)/len(lis)
+	print "Cost",cost
 	return cost
 
 def regress(lis):
-	theta0=random.randint(1,100)
-	theta1=random.randint(1,100)
-	alpha=float(100)
+	'''actual regression is taking place here'''
+	#initalized the theta0 and theta1 values
+	theta0=0
+	theta1=0
+	#alpha is the learning rate
+	alpha=float(.002)
+	#incremental cost to stop the iterations
 	inc_cost=20
 	temp1=0
 	temp2=0
-	while inc_cost>1 or inc_cost<-1:
-		print 10
+	count=0
+	while inc_cost>.1:
+		count=count+1
 		for x in range(len(lis)):
-			temp1+=-(theta0+theta1*lis[x][0]-lis[x][1])/len(lis)
-			temp2+=-(theta0+theta1*lis[x][0]-lis[x][1])*lis[x][1]/len(lis)
+			#temporary variables to store new theta0 and theta1, differentiated cost wrt theta0 and theta1
+			temp1+=(theta0+theta1*lis[x][0]-lis[x][1])/float(len(lis))
+			temp2+=((theta0+theta1*lis[x][0]-lis[x][1])*lis[x][0])/float(len(lis))
+		print "temp1", temp1
+		print "temp2", temp2
+		#changed theta0, theta1
 		temp1_=theta0-alpha*temp1
 		temp2_=theta1-alpha*temp2
-		inc_cost=cost(lis,theta0,theta1)-cost(lis,temp1_,temp2_)
+		inc_cost=(cost(lis,theta0,theta1)-cost(lis,temp1_,temp2_))
+		if inc_cost>=0:
+			inc_cost=inc_cost
+		else:
+			inc_cost=-inc_cost
 		theta0=temp1_
 		theta1=temp2_
-		print inc_cost
-		print "hehr"
+		print "inc_cost",inc_cost
+		print '\n'
+	print "count",count
 	return [theta0,theta1]
 
 def price(size):
-	a=GenerateData()
-	a.initialize(2)
-	learn=a.data
+	#a=GenerateData()
+	#a.initialize(20)
+	#learn=a.data
+	learn=[(x,x) for x in range(10)]
+	print learn
 	b=regress(learn)
 	price_house=b[0]+b[1]*size
 	return price_house
 
-print price(10)
+print price(20),"price"
 
 
 
